a528e8d07573be6068298cefc35fcb12c9518663
wjntx <wjntx@rams.colostate.edu>
2020-07-16 18:54:17 -0600

    Updates

---
+++ precache-manifest.22895c8a4d1cad0720406030f91720a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb9111a6fc42b9613a5a1e4485a5f89",
+    "url": "/mgweb/index.html"
+  },
+  {
+    "revision": "f83453a7f46356d4688e",
+    "url": "/mgweb/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1115df9bb8dc132334ab",
+    "url": "/mgweb/static/js/2.36423a72.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mgweb/static/js/2.36423a72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83453a7f46356d4688e",
+    "url": "/mgweb/static/js/main.477de66a.chunk.js"
+  },
+  {
+    "revision": "fb4b2eaa4af51192ddc4",
+    "url": "/mgweb/static/js/runtime-main.47e4843a.js"
+  },
+  {
+    "revision": "ee87f75ce043afaf2e0c89898d49ca2d",
+    "url": "/mgweb/static/media/submarine.ee87f75c.jpg"
+  },
+  {
+    "revision": "a3d905d542867dd52ffe8afabec0fd9a",
+    "url": "/mgweb/static/media/train.a3d905d5.jpg"
+  },
+  {
+    "revision": "d585ecb3f0a2f1bf40afeb358cb66822",
+    "url": "/mgweb/static/media/williams.d585ecb3.jpg"
+  }
+]);
