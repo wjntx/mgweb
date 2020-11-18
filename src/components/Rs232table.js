daca7cc515700c1c4feed15c4bd4fc4d309bd5fc
wjntx <42551951+wjntx@users.noreply.github.com>
2020-11-18 13:58:12 -0700

    Updated Table

--- src/components/Rs232table.js
+++ src/components/Rs232table.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import Table from 'react-bootstrap/Table';
 import styled from 'styled-components';
 
 const Styles = styled.div`
@@ -52,11 +53,118 @@ p {
         color: #0000EE;
     }
 }
+
+.bgcolor {
+    background-color: grey;
+}
 `;
 
 export const Rs232table = () => (
     <React.Fragment>
         <Styles>
+        <h2>RS-232 DB-25 Male DTE</h2>
+            <Table striped bordered hover size="sm">
+                <thead>
+                    <tr>
+                        <th>Product Description</th>
+                        <th>Electrical Description</th>
+                        <th>Pin #</th>
+                        <th>Direction</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Earth/Shield Ground</td>
+                        <td>--</td>
+                        <td>1</td>
+                        <td>--</td>
+                    </tr>
+                    <tr>
+                        <td>TxD, Transmit Data</td>
+                        <td>RS-232/V.28</td>
+                        <td>2</td>
+                        <td>Output</td>
+                    </tr>
+                    <tr>
+                        <td>RxD, Receive Data</td>
+                        <td>RS-232/V.28</td>
+                        <td>3</td>
+                        <td>Input</td>
+                    </tr>
+                    <tr>
+                        <td>RTS, Request To Send</td>
+                        <td>RS-232/V.28</td>
+                        <td>4</td>
+                        <td>Output</td>
+                    </tr>
+                    <tr>
+                        <td>CTS, Clear To Send</td>
+                        <td>RS-232/V.28</td>
+                        <td>5</td>
+                        <td>Input</td>
+                    </tr>
+                    <tr>
+                        <td>DSR, Data Carrier Detect</td>
+                        <td>RS-232/V.28</td>
+                        <td>6</td>
+                        <td>Input</td>
+                    </tr>
+                    <tr>
+                        <td>Signal Ground</td>
+                        <td>--</td>
+                        <td>7</td>
+                        <td>--</td>
+                    </tr>
+                    <tr>
+                        <td>DCD, Data Carrier Detect</td>
+                        <td>RS-232/V.28</td>
+                        <td>8</td>
+                        <td>Input</td>
+                    </tr>
+                    <tr>
+                        <td>TxC, Transmit Clock</td>
+                        <td>RS-232/V.28</td>
+                        <td>9</td>
+                        <td>Input</td>
+                    </tr>
+                    <tr>
+                        <td>RxC, Receive Clock</td>
+                        <td>RS-232/V.28</td>
+                        <td>17</td>
+                        <td>Input</td>
+                    </tr>
+                    <tr>
+                        <td>LL, Local Loopback Control</td>
+                        <td>RS-232/V.28</td>
+                        <td>18</td>
+                        <td>Output</td>
+                    </tr>
+                    <tr>
+                        <td>DTR, Data Terminal Ready</td>
+                        <td>RS-232/V.28</td>
+                        <td>20</td>
+                        <td>Output</td>
+                    </tr>
+                    <tr>
+                        <td>RL, Remote Loopback Control</td>
+                        <td>RS-232/V.28</td>
+                        <td>21</td>
+                        <td>Output</td>
+                    </tr>
+                    <tr>
+                        <td>RI, Ring Indicator</td>
+                        <td>RS-232/V.28</td>
+                        <td>22</td>
+                        <td>Input</td>
+                    </tr>
+                    <tr>
+                        <td>AuxClk, DTE Clock Output</td>
+                        <td>RS-232/V.28</td>
+                        <td>24</td>
+                        <td>Output</td>
+                    </tr>
+                </tbody>
+            </Table>
         <Container>
             <h2>RS-232 DB-25 Male DTE</h2>
             <hr />
